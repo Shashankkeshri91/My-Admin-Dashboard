3852934d165391ca532fe39100d2cc04432dbe32
Shashank keshri <99127554+Shashankkeshri91@users.noreply.github.com>
2023-08-10 09:58:03 +0530

    Completed Dashboard Components

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,7 +1,32 @@
-import { Card, Space, Statistic, Table, Typography } from 'antd'
-import {ShoppingCartOutlined,DollarCircleOutlined,UserOutlined,ShoppingOutlined} from "@ant-design/icons"
-import { useEffect, useState } from 'react';
-import { getCustomers, getInventory, getOrders } from "../../API";
+import {
+  DollarCircleOutlined,
+  ShoppingCartOutlined,
+  ShoppingOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { Card, Space, Statistic, Table, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { getCustomers, getInventory, getOrders, getRevenue } from "../../API";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 function Dashboard() {
   const [orders, setOrders] = useState(0);
@@ -89,21 +114,21 @@ function Dashboard() {
       </Space>
       <Space>
         <RecentOrders />
-        {/* <DashboardChart /> */}
+        <DashboardChart />
       </Space>
     </Space>
   );
 }
 
-function DashboardCard({title,value,icon}){
-    return(
-        <Card>
-        <Space direction='horizontal'>
+function DashboardCard({ title, value, icon }) {
+  return (
+    <Card>
+      <Space direction="horizontal">
         {icon}
-            <Statistic title={title} value={value} />
-            </Space>
-        </Card>
-    );
+        <Statistic title={title} value={value} />
+      </Space>
+    </Card>
+  );
 }
 function RecentOrders() {
   const [dataSource, setDataSource] = useState([]);
@@ -143,4 +168,53 @@ function RecentOrders() {
   );
 }
 
+function DashboardChart() {
+  const [reveneuData, setReveneuData] = useState({
+    labels: [],
+    datasets: [],
+  });
+
+  useEffect(() => {
+    getRevenue().then((res) => {
+      const labels = res.carts.map((cart) => {
+        return `User-${cart.userId}`;
+      });
+      const data = res.carts.map((cart) => {
+        return cart.discountedTotal;
+      });
+
+      const dataSource = {
+        labels,
+        datasets: [
+          {
+            label: "Revenue",
+            data: data,
+            backgroundColor: "rgba(255, 0, 0, 1)",
+          },
+        ],
+      };
+
+      setReveneuData(dataSource);
+    });
+  }, []);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: true,
+        text: "Order Revenue",
+      },
+    },
+  };
+
+  return (
+    <Card style={{ width: 500, height: 250 }}>
+      <Bar options={options} data={reveneuData} />
+    </Card>
+  );
+}
 export default Dashboard;
