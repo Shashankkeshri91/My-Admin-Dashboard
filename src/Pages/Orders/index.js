17139a3ec080efc77b4604106e8544bce40cc301
Shashank keshri <99127554+Shashankkeshri91@users.noreply.github.com>
2023-08-16 09:10:46 +0530

    Uploading My Updated Files

--- src/Pages/Orders/index.js
+++ src/Pages/Orders/index.js
@@ -1,12 +1,54 @@
-import { Typography } from 'antd'
-import React from 'react'
+import { Avatar, Rate, Space, Table, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { getInventory, getOrders } from "../../API";
 
-const Orders = () => {
+function Orders() {
+  const [loading, setLoading] = useState(false);
+  const [dataSource, setDataSource] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    getOrders().then((res) => {
+      setDataSource(res.products);
+      setLoading(false);
+    });
+  }, []);
+
   return (
-    <div>
-       <Typography.Title level={4}>Orders</Typography.Title>
-    </div>
-  )
+    <Space size={20} direction="vertical">
+      <Typography.Title level={4}>Orders</Typography.Title>
+      <Table
+        loading={loading}
+        columns={[
+          {
+            title: "Title",
+            dataIndex: "title",
+          },
+          {
+            title: "Price",
+            dataIndex: "price",
+            render: (value) => <span>${value}</span>,
+          },
+          {
+            title: "DiscountedPrice",
+            dataIndex: "discountedPrice",
+            render: (value) => <span>${value}</span>,
+          },
+          {
+            title: "Quantity",
+            dataIndex: "quantity",
+          },
+          {
+            title: "Total",
+            dataIndex: "total",
+          },
+        ]}
+        dataSource={dataSource}
+        pagination={{
+          pageSize: 5,
+        }}
+      ></Table>
+    </Space>
+  );
 }
-
-export default Orders
+export default Orders;
